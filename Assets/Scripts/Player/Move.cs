29b64fd3f468a28a75b54439932f779e97f3ae6f
ryfprivate <ryfprivate@outlook.com>
2020-12-30 00:02:24 +1100

    Added boundaries to shapeship movement

--- Assets/Scripts/Player/Move.cs
+++ Assets/Scripts/Player/Move.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     private Vector3 mousePos;
-    private float speed = 0.01f;
+    private float speed = 0.05f;
+    private float maxX = 8;
+    private float maxY = 4;
 
     void Start()
     {
@@ -17,7 +19,25 @@ public class Move : MonoBehaviour
     {
         if (Input.GetMouseButton(0)) {
             mousePos = Input.mousePosition;
+
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            float newX = mousePos.x;
+            float newY = mousePos.y;
+            float newZ = mousePos.z;
+            if (mousePos.x > maxX) {
+                newX = maxX;
+            }
+            if (mousePos.x < -maxX) {
+                newX = -maxX;
+            }
+            if (mousePos.y > maxY) {
+                newY = maxY;
+            }
+            if (mousePos.y < -maxY) {
+                newY = -maxY;
+            }
+            mousePos = new Vector3(newX, newY, newZ);
+
             transform.position = Vector2.Lerp(transform.position, mousePos, speed);
         }
     }
