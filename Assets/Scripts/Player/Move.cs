4cb87184c616b052fa769d0c79568a7174219268
ryfprivate <ryfprivate@outlook.com>
2020-12-29 23:18:14 +1100

    feature: player follow mouse

---
+++ Assets/Scripts/Player/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private float speed = 0.01f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.position = Vector2.Lerp(transform.position, mousePos, speed);
+        }
+    }
+}
