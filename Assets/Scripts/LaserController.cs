b87c4731546e077becea7b461bf89dcc03171b63
ryfprivate <ryfprivate@outlook.com>
2020-12-30 17:41:36 +1100

    Added laser prefab

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float speed;
+    void Start()
+    {
+        speed = 5;
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+    }
+}
