6e199242cd7ca5fde102c69a1a3a0277790f67ce
ryfprivate <ryfprivate@outlook.com>
2021-01-01 23:45:16 +1100

    (new) Added moving waves

---
+++ Assets/Scripts2/WaveMoving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMoving : Wave
+{
+
+    private float waveTime = 10f;
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        targetPosition = new Vector3(0, -6, 0);
+        StartCoroutine(MoveToPosition());
+
+    }
+
+    public IEnumerator MoveToPosition()
+    {
+        Vector3 currentPosition = transform.position;
+        float t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / waveTime;
+            transform.position = Vector3.Lerp(currentPosition, targetPosition, t);
+            yield return null;
+        }
+    }
+}
