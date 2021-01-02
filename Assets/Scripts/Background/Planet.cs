cc6cf741281e903356d8d3d0e071ccecc8f3a488
ryfprivate <ryfprivate@outlook.com>
2021-01-03 07:36:55 +1100

    (new) Added infinite scrolling stars and planets

---
+++ Assets/Scripts/Background/Planet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public float speed;
+    public bool isMoving;
+    Vector2 min;
+    Vector2 max;
+
+    void Awake()
+    {
+        isMoving = false;
+
+        min = Camera.main.ViewportToWorldPoint(new Vector2(0, -0.2f));
+        max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1.2f));
+
+        max.y = max.y + GetComponent<SpriteRenderer>().sprite.bounds.extents.y;
+        min.y = min.y - GetComponent<SpriteRenderer>().sprite.bounds.extents.y;
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (!isMoving) return;
+
+        Vector2 position = transform.position;
+
+        position = new Vector2(position.x, position.y + speed * Time.deltaTime);
+
+        transform.position = position;
+
+        if (transform.position.y < min.y)
+        {
+            isMoving = false;
+        }
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = new Vector2(Random.Range(min.x, max.x), max.y);
+    }
+}
---
+++ Assets/Scripts/Background/PlanetController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetController : MonoBehaviour
+{
+    public GameObject[] Planets;
+    Queue<GameObject> availablePlanets = new Queue<GameObject>();
+
+    void Start()
+    {
+        availablePlanets.Enqueue(Planets[0]);
+        availablePlanets.Enqueue(Planets[1]);
+        availablePlanets.Enqueue(Planets[2]);
+
+        InvokeRepeating("MovePlanetDown", 0, 20f);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void MovePlanetDown()
+    {
+        EnqueuePlanets();
+
+        if (availablePlanets.Count == 0) return;
+
+        GameObject planet = availablePlanets.Dequeue();
+
+        planet.GetComponent<Planet>().isMoving = true;
+    }
+
+    void EnqueuePlanets()
+    {
+        foreach (GameObject planet in Planets)
+        {
+            if ((planet.transform.position.y < 0) && (!planet.GetComponent<Planet>().isMoving))
+            {
+                planet.GetComponent<Planet>().ResetPosition();
+
+                availablePlanets.Enqueue(planet);
+            }
+        }
+    }
+}
