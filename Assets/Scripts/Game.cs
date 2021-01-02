a023e80ccf42e913619eb27ce7c89e07a668dd3e
ryfprivate <ryfprivate@outlook.com>
2021-01-03 09:10:35 +1100

    (new) Added two diagonal wave prefabs

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public static int roundLength = 20;
-    public static float waveTime = 5f;
+    public static float waveTime = 10f;
     public static float currRadiation;
     public static float maxRadiation;
 
